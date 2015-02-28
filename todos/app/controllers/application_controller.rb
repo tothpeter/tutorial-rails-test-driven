b74a3f59682648a3e3d380deefeed23ab230ad2b
Peter Toth <tothpeter08@gmail.com>
2015-02-28 18:39:42 +0100

    Extract session[:email] to app controller

--- todos/app/controllers/application_controller.rb
+++ todos/app/controllers/application_controller.rb
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
   end
 
   def signed_in?
-    session[:current_email].present?
+    current_email.present?
+  end
+
+  def current_email
+    session[:current_email]
   end
 end
